1c5b9f53d87dec60a937e8ee0a1e76d86a0b8f0b
Chris Winter <chris-winter@live.de>
2021-03-20 09:33:30 +0100

    added plex support

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@ app_name="plexaria"
 version="v. 0.1"
 created_by="@thedevofthings"
 
+#special case config
+obscure_password=false
+
 #welcome
 echo "------------------------------------------"
 echo "            __                    _       "
@@ -11,17 +14,22 @@ echo "   / __ \/ / _ \| |/_/ __  / ___/ / __  / "
 echo "  / /_/ / /  __/>  </ /_/ / /  / / /_/ /  "
 echo " / .___/_/\___/_|_|\__,_/__/  /_/\__,_/   "
 echo "/_/                                       "
-
 echo $version
 echo "by" $created_by
-echo "-----------------------------------------"
+echo "------------------------------------------"
 sleep 1
 
 #download required packages and other stuff
-echo -e "\033[31mupdate package list and get rclone"
+echo -e "\033[31mupdate package list and get rclone and plex"
 sleep 1
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | apt key add -
+echo deb https://downloads.plex.tv/repo/deb public main | tee /etc/apt/sources.list.d/plexmediaserver.list
 apt update -y
-apt install rclone -y
+apt install rclone plexmediaserver -y
+echo -e "\033[31mdownloading service files"
+sleep 1
+wget https://raw.githubusercontent.com/devofthings/plexaria/main/plexaria_movies.service
+wget https://raw.githubusercontent.com/devofthings/plexaria/main/plexaria_series.service
 
 #create essential media folders
 echo -e "\033[31mcreating media folders"
@@ -36,7 +44,7 @@ dir_gseries="/media/gdrive/gSeries"
 [ ! -d $dir_gseries ] && mkdir -p $dir_gseries
 
 #setup gdrive via rclone
-rclone config create "gdrive" "drive" config_is_local "false"
+#rclone config create "gdrive" "drive" config_is_local "false"
 
 echo -e "\033[31myou need to provide two passwords to encrypt your files on gdrive"
 echo -e "\033[32mplease provide the first password and press [ENTER]:"
@@ -49,5 +57,11 @@ echo -e "\033[31msecond password:" $second_password
 echo -e "\033[37mmake sure that you don't lose these passwords!"
 sleep 1
 
-rclone config create "gcrypt" "crypt" remote "gdrive:" filename_encryption "standard" directory_name_encryption "true" password $first_password password2 $second_password --no-obscure
+if [ $obscure_password ]
+then
+    rclone config create "gcrypt" "crypt" remote "gdrive:" filename_encryption "standard" directory_name_encryption "true" password $first_password password2 $second_password
+else
+    rclone config create "gcrypt" "crypt" remote "gdrive:" filename_encryption "standard" directory_name_encryption "true" password $first_password password2 $second_password --no-obscure
+fi
+
 echo -e "\033[37mdone."
